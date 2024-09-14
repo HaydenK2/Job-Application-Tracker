2d0f4c5d77da99923afe8e9ecdfd0d6a2cd7d1a8
Hayden <haydenk643@gmail.com>
2024-09-14 02:27:21 -0500

    updated header no white top

--- job-app-viewer/src/api/axiosConfig.js
+++ job-app-viewer/src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://cf4b-75-22-141-193.ngrok-free.app',
+    // baseURL:'https://cf4b-75-22-141-193.ngrok-free.app',
+    baseURL:'https://c24a-76-191-29-33.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
